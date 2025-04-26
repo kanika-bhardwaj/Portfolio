79b97ed634125644e9ce5fb90e30421f321a8b17
kanika-bhardwaj <90175293+kanika-bhardwaj@users.noreply.github.com>
2025-04-26 17:36:02 +0000

    removed unnecessary things

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.nav-links a').forEach(link => {
-    link.addEventListener('click', (e) => {
-      e.preventDefault();
-      const section = document.querySelector(link.getAttribute('href'));
-      section.scrollIntoView({ behavior: 'smooth' });
-    });
-  });
